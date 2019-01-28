d62890bc939b457f872ad53e9f32e389dcaa7182
kdy <laasl@naver.com>
2019-01-28 12:19:56 +0900

    레귤러 익스프레션 함수

--- pathapi.py
+++ pathapi.py
@@ -13,8 +13,55 @@ def project(path):
 		return "", "경로에서 project 정보를 가지고 올 수 없습니다."
 	return p[0], None
 
-if __name__ == "__main__":
-	projectName, err = project(path)
-	if err:	
-		print err 
-	print projectName
+def seq(path):
+	"""
+	경로를 넣으면 seq 이름을 반환한다.
+	"""
+	p = re.findall('/shot/(\S[^/]+)', path.replace("\\","/"))
+	if len(p) != 1:
+		return "", "경로에서 seq 정보를 가지고 올 수 없습니다"
+	return p[0], None
+
+def shot(path):
+	"""
+	경로를 넣으면 shot 이름을 반환한다.
+	"""
+	p = re.findall('/shot/\w+/(\w+)', path.replace("\\","/"))
+	if len(p) != 1:
+		return "", "경로에서 shot 정보를 가지고 올 수 없습니다"
+	return p[0], None
+	
+def task(path):
+	"""
+	경로를 넣으면 task 이름을 반환한다.
+	"""
+	p = re.findall('/shot/\w+/\w+/(\w+)', path.replace("\\","/"))
+	if len(p) != 1:
+		return "", "경로에서 task 정보를 가지고 올 수 없습니다"
+	return p[0], None
+
+def ver(path):
+	"""
+	경로를 넣으면  version을 반환한다.
+	"""
+	p = re.findall("_v(\d+)",path)
+	if len(p) != 1:
+		return -1, "경로에서 버전 정보를 가지고 올 수 없습니다"
+	return int(p[0]), None
+
+def seqnum(path):
+	"""
+	경로를 넣으면 seq number를 반환한다.
+	"""
+	p = re.findall('\.(\d+)\.', path.replace("\\","/"))
+	if len(p) != 1:
+		return -1, "경로에서 seq number 정보를 가지고 올 수 없습니다"
+	return int(p[0]), None
+
+#test.py가 없다면?
+#if __name__ == "__main__":
+	#path = "/project/circle/shot/FOO/0010/comp/FOO_0010_comp_v001.nk"
+	#projectName, err = project(path)
+	#if err:
+		#print err
+	#print projectName
